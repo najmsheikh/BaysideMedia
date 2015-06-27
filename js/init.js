66a6ded876d11d6549f2595beba68d8877406ea4
Najm Sheikh <najm97@gmail.com>
2015-06-27 15:32:03 -0400

    Added POST for contact form

--- js/init.js
+++ js/init.js
@@ -5,5 +5,28 @@
     $('.parallax').parallax();
     $('.scrollspy').scrollSpy();
 
+    var fname, lname, email, messsage;
+    $("input:radio[name=group1]").click(function() {
+        subject = $(this).val();
+    });
+    $("#submit-button").click(function() {
+        fname = $("#first_name").val();
+        lname = $("#last_name").val();
+        email = $("#email").val();
+        message = $("#message").val();
+        $.post("https://tranquil-cove-4564.herokuapp.com/sendmsg", {
+            furstname: name,
+            lastname: phone,
+            email: subject,
+            msg: message
+        }, function(data) {
+            Materialize.toast('Your message has been delivered!', 4000);
+            $("#first_name").val('');
+            $("#last_name").val('');
+            $("#email").val('');
+            $("#message").val('');
+        });
+    });
+
   });
 })(jQuery);
